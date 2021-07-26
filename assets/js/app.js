56913caa22870fc010fe55cfa81fed9ead102703
mao <mao@tigermedia.dk>
2021-07-26 15:20:03 +0200

    added scroll to anchor + fixed top navbar + jquery library

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,3 +23,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
+
+$(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+        var target = $(this.hash);
+        if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+        if (target.length == 0) target = $('html');
+        $('html, body').animate({ scrollTop: target.offset().top-75 }, 1000);
+        return false;
+    });
+});
